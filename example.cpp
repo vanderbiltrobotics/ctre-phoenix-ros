35988dc98ce96b0d6eaa45b764a45e66301845f7
Joey Holliday <joeyholliday77@gmail.com>
2018-11-15 19:01:26 -0600

    Use geometry_msgs/Twist instead of sensor_msgs/Joy

--- example.cpp
+++ example.cpp
@@ -8,7 +8,7 @@
 #include "Platform-linux-socket-can.h"
 
 #include "ros/ros.h"
-#include "sensor_msgs/Joy.h"
+#include "geometry_msgs/Twist.h"
 
 using namespace ctre::phoenix;
 using namespace ctre::phoenix::platform;
@@ -23,13 +23,13 @@ VictorSPX* fr = nullptr;
 VictorSPX* bl = nullptr;
 VictorSPX* br = nullptr;
 
-void joyCallback(const sensor_msgs::Joy::ConstPtr& msg)
+void cmdCallback(const geometry_msgs::Twist::ConstPtr& msg)
 {
   ctre::phoenix::platform::FeedWatchDog(100);
-  float moveValue = msg->axes[MOVE];
-  float rotateValue = msg->axes[ROTATE];
-  float leftMotorOutput = 0.0;
-  float rightMotorOutput = 0.0;
+  double moveValue = msg->linear.x;
+  double rotateValue = msg->angular.z;
+  double leftMotorOutput = 0.0;
+  double rightMotorOutput = 0.0;
   if (moveValue > 0.0) {
     if (rotateValue > 0.0) {
       leftMotorOutput = moveValue - rotateValue;
@@ -57,7 +57,7 @@ void joyCallback(const sensor_msgs::Joy::ConstPtr& msg)
 int main(int argc, char **argv) {
     ros::init(argc, argv, "motor_controller");
     ros::NodeHandle n;
-    ros::Subscriber sub = n.subscribe("joy", 1000, joyCallback);
+    ros::Subscriber sub = n.subscribe("cmd_vel", 1000, cmdCallback);
 
     std::string interface = "can0";
     ctre::phoenix::platform::can::SetCANInterface(interface.c_str());
